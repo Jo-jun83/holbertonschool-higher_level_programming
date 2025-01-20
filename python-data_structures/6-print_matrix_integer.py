d0e9dda91fed02361a2f14f460c4e615298ec4c2
Jonas-Jungling <jonas.jungling@oclock.school>
2025-01-20 14:07:57 +0100

    add function print_matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """
+    Prints a matrix of integers.
+
+    Args:
+        matrix (list of list of int): A 2D list where each
+        sublist represents a row of the matrix.
+
+    Returns:
+        None
+    """
+    for row in matrix:
+        for i in range(len(row)):
+            if i < len(row) - 1:
+                print("{}".format(row[i]), end=" ")
+            else:
+                print("{}".format(row[i]), end="")
+        print()
