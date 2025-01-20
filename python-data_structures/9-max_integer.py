9d6cdf6c55f0d29cbe2aedf8696febdcbeee7cf6
Jonas-Jungling <jonas.jungling@oclock.school>
2025-01-20 14:42:53 +0100

    add function max_integer

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """
+    Finds the maximum integer in a list.
+
+    Args:
+        my_list (list): A list of integers. Defaults to an empty list.
+
+    Returns:
+        int: The maximum integer in the list. If the list is empty,
+        returns None.
+    """
+    max_num = my_list[0]
+    if len(my_list) == 0:
+        return None
+    else:
+        for i in my_list:
+            if i > max_num:
+                max_num = i
+        return max_num
