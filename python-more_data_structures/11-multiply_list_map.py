900a36c1d8934621841692802ce8c398d266b80b
Jonas-Jungling <jonas.jungling@oclock.school>
2025-01-22 10:25:55 +0100

    add function multiply_list_map

---
+++ python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def multiply_list_map(my_list=[], number=0):
+    """
+    Multiplies each element of a list by a given number using the map function.
+
+    Args:
+        my_list (list): The list of integers to be multiplied.
+        number (int): The number by which each element of the list
+        will be multiplied.
+
+    Returns:
+        list: A new list with each element multiplied by the given number.
+    """
+    def multiply(num):
+        return num * number
+
+    res = list(map(multiply, my_list))
+    return res
