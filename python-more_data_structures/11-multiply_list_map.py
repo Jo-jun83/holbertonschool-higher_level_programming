77dc5bf7b99e05bd1d4d97dc980085229b5e3071
Jonas-Jungling <jonas.jungling@oclock.school>
2025-01-23 09:40:43 +0100

    refactor function multiply

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    """
-    Multiplies each element of a list by a given number using the map function.
-
-    Args:
-        my_list (list): The list of integers to be multiplied.
-        number (int): The number by which each element of the list
-        will be multiplied.
-
-    Returns:
-        list: A new list with each element multiplied by the given number.
-    """
-    res = list(map(lambda x: x * number, my_list))
-    return res
+    return list(map(lambda x: x * number, my_list))
