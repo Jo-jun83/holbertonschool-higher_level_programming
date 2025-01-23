ee34a593f36ca75b62289f1d72f7701003a8bce1
Jonas-Jungling <jonas.jungling@oclock.school>
2025-01-23 21:02:58 +0100

    add function safe_print_division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,16 +4,20 @@ def safe_print_division(a, b):
     """
     Divides two numbers and prints the result.
 
-    This function takes two arguments, `a` and `b`, attempts to divide `a` by `b`, 
-    and prints the result of the division. If the division is successful, it returns 
-    the result. If a division by zero occurs, it prints a message and returns None.
+    This function takes two arguments, `a` and `b`, attempts
+    to divide `a` by `b`,
+    and prints the result of the division. If the division
+    is successful, it returns
+    the result. If a division by zero occurs, it prints a
+    message and returns None.
 
     Args:
         a (int or float): The numerator.
         b (int or float): The denominator.
 
     Returns:
-        float or None: The result of the division if successful, otherwise None.
+        float or None: The result of the division if successful,
+        otherwise None.
     """
     try:
         result = a / b
@@ -22,6 +26,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
     return result
-
-
-    
