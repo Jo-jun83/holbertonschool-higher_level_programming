7b8eec4662e8d8e03a36e3fee0213e593868fa8a
Jonas-Jungling <jonas.jungling@oclock.school>
2025-01-23 21:45:13 +0100

    Add function raise_exception

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    """
+    Raises a TypeError exception.
+
+    This function does not take any arguments and raises a TypeError
+    exception when called. It can be used to demonstrate exception
+    handling in Python.
+    """
+    raise TypeError("Error")
