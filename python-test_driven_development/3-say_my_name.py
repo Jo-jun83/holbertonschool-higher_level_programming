d9dd9159f74c5bb26adeb3c049bd99cf398db0bd
Jonas-Jungling <jonas.jungling@oclock.school>
2025-01-25 21:02:41 +0100

    add tests function sy_my_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,18 +4,19 @@
 This module print a string "My name is <first name> <lastname>
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
     Prints "My name is <first name> <last name>".
 
     Args:
         first_name (str): The first name to be printed.
-        last_name (str, optional): The last name to be printed. Defaults to an empty string.
+        last_name (str, optional): The last name to be printed.
+        Defaults to an empty string.
     """
-
     if type(first_name) not in [str]:
-            raise TypeError("first_name must be a string")
+        raise TypeError("first_name must be a string")
     if type(last_name) not in [str]:
-            raise TypeError("last_name must be a string")
+        raise TypeError("last_name must be a string")
     else:
         print("My name is {} {}".format(first_name, last_name))
