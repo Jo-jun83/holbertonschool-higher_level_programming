e16373c5dc9b608f90add1f1480dae96bff04425
Jonas-Jungling <jonas.jungling@oclock.school>
2025-01-29 10:07:42 +0100

    refactor function init

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -33,8 +33,8 @@ class Rectangle:
 
     """this class define a rectangle"""
     def __init__(self, width=0, height=0, print_symbol="#"):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
