b50db9a5ba430584bbab3695462385ae8e296546
Jonas-Jungling <jonas.jungling@oclock.school>
2025-01-30 09:44:51 +0100

    refactor function my_print

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -85,11 +85,11 @@ class Square:
             self.__position = value
 
     def my_print(self):
-    """Prints the square with the character #"""
-    if self.__size == 0:
-        print()
-        return
-    for _ in range(self.__position[1]):
-        print()
-    for _ in range(self.__size):
-        print(" " * self.__position[0] + "#" * self.__size)
+        """Prints the square with the character #"""
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__position[1]):
+            print()
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
