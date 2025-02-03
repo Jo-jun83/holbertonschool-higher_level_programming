fbf2d3172b546e1f115103d961857416c544de9f
Jonas-Jungling <jonas.jungling@oclock.school>
2025-02-03 14:57:34 +0100

    add docstring for module in all the project

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module provides a function to check if an object is an instance
+or an inherited instance of a specified class.
+"""
+
 
 def is_kind_of_class(obj, a_class):
     """
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This module defines a single function `inherits_from`."""
+
 
 def inherits_from(obj, a_class):
     """
