88f35855179c8c5bea948ad70511be09ba02e0aa
Jonas-Jungling <jonas.jungling@oclock.school>
2025-02-18 15:06:05 +0100

    refactor task_03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -40,7 +40,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.wfile.write(b'Ok')
+            self.wfile.write(b'OK')
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
