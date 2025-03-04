38fbd6517823d7070fb671955b12086e0645c54b
Jonas-Jungling <jonas.jungling@oclock.school>
2025-03-04 09:42:31 +0100

    refactor some queries

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
--- This SQL query updates the 'score' column to 10 in 'second_table' where the current 'score' is 14.
+-- This SQL query updates the 'score' column to 10 in 'second_table' where the current 'name' is Bob.
 UPDATE second_table SET score = 10 WHERE name = 'Bob;
