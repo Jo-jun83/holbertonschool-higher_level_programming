e580d2f6211384d977aeb8b4316e3a0e88fa70c8
Jonas-Jungling <jonas.jungling@oclock.school>
2025-03-04 09:40:58 +0100

    refactor some queries

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,2 @@
 -- This SQL query selects the score and counts the number of occurrences of each score from the second_table, grouping the results by score.
-SELECT score, COUNT(*) AS number
-FROM second_table
-GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- This SQL query counts the number of non-NULL 'id' values in 'first_table'.
-SELECT COUNT(id) FROM first_table;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
