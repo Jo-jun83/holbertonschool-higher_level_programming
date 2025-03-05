69bb9d5ce2d01bda1e5c110ecb0188fc02500910
Jonas-Jungling <jonas.jungling@oclock.school>
2025-03-05 10:53:50 +0100

    Add script that lists all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,21 +3,22 @@
 This module connects to a MySQL database and retrieves data from
 the 'states' table.
 """
-
-
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(
-    host="localhost",
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3],
-    port=3306
-)
 
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY states.id")
-results = cursor.fetchall()
-for result in results:
-    print(result)
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
