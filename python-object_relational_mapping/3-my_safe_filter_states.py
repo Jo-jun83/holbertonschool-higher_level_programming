4f223e179beeafe5f4dc4d5896725ea09dd36f46
Jonas-Jungling <jonas.jungling@oclock.school>
2025-03-05 14:39:46 +0100

    Add script that takes in an argument and displays all values safe from mysql injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+This module connects to a MySQL database and retrieves data from
+the 'states' table.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=user,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE CONVERT(name USING Latin1) \
+        COLLATE Latin1_General_CS = '{}' \
+        ORDER BY states.id;".format(sys.argv[4],)
+    )
+    results = cursor.fetchall()
+    for result in results:
+        print("{}".format(result))
