e161222939684f3c87bfe344a23741c761fa4fa5
Jonas-Jungling <jonas.jungling@oclock.school>
2025-03-06 15:53:23 +0100

    Add script that prints all City objects from the database

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -18,3 +18,4 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True,
                 nullable=False)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state")
