e5475c18351ee5f4155180a5fd6232b4e9b7e6e7
Jonas-Jungling <jonas.jungling@oclock.school>
2025-03-21 22:46:45 +0100

    Extending Dynamic Data Display to Include SQLite in Flask

---
+++ python-server_side_rendering/task_04_db.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+from flask import Flask, render_template, request
+from json import load
+import json, csv
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    with open('items.json') as f:
+        items = load(f).get('items') or []
+    return render_template('items.html', items=items)
+
+@app.route('/products/')
+def products():
+    source = request.args.get("source")
+    product_id = request.args.get("id")
+    if source == "json":
+        with open('products.json') as f:
+            data = load(f)
+            products = data
+    elif source == "csv":
+        with open('products.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            products = [row for row in reader]
+    
+    elif source == "sql":
+        conn = sqlite3.connect("products.db")
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM products")
+        products = cursor.fetchall()
+        conn.close()
+    else:
+        return render_template('product_display.html',
+                               error_message="Wrong source")
+
+    if product_id:
+        filtered_products = [product for product in products if str(
+            product['id']) == product_id]
+        if not filtered_products:
+            return render_template('product_display.html',
+                                   error_message="Product not found")
+        products = filtered_products
+    return render_template('product_display.html', products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
